0342388217f01074a203b82df92fc4935cd9be31
Jacob Highfield <camperboy1000@gmail.com>
2023-07-26 19:33:06 -0400

    Add more nvim plugin packs

--- dot_config/astronvim/lua/user/plugins.lua
+++ dot_config/astronvim/lua/user/plugins.lua
@@ -3,8 +3,13 @@ local plugins = {
 		"AstroNvim/astrocommunity",
 		{ import = "astrocommunity.colorscheme.catppuccin" },
 		{ import = "astrocommunity.pack.lua" },
+		{ import = "astrocommunity.pack.json" },
+		{ import = "astrocommunity.pack.yaml" },
+		{ import = "astrocommunity.pack.toml" },
 		{ import = "astrocommunity.pack.rust" },
 		{ import = "astrocommunity.pack.python" },
+		{ import = "astrocommunity.pack.go" },
+		{ import = "astrocommunity.pack.docker" },
 		{ import = "astrocommunity.pack.ansible" },
 	},
 	{
